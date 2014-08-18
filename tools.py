1a0a7e0fd50c2c7f4575ba94afe199378e13c945
gaoguanyu <springaction@foxmail.com>
2014-08-18 15:13:33 +0800

    dd

--- tools.py
+++ tools.py
@@ -1,12 +1,17 @@
 import hashlib
 import pycurl
 import cStringIO
+from random import randrange
 
 while True:
+    i = randrange(190)
+    no = "%03d" % i
+    url = 'http://155.69.149.26/dream_700_700_1k_' + no + '.ts'
+    print url
     buf = cStringIO.StringIO()
     c = pycurl.Curl()
     c.setopt(pycurl.FOLLOWLOCATION, 1)
-    c.setopt(c.URL, 'http://155.69.149.26/dream_700_700_1k_012.ts')
+    c.setopt(c.URL, url)
     c.setopt(c.WRITEFUNCTION, buf.write)
     c.perform()
 
