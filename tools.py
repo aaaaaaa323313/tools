23637c7d2096e260d8ce31e66a78c12b6fb3fc12
gaoguanyu <springaction@foxmail.com>
2014-08-18 14:39:19 +0800

    dd

--- tools.py
+++ tools.py
@@ -2,14 +2,15 @@ import hashlib
 import pycurl
 import cStringIO
 
-buf = cStringIO.StringIO()
-c = pycurl.Curl()
-c.setopt(pycurl.FOLLOWLOCATION, 1)
-c.setopt(c.URL, 'http://155.69.149.26/dream_700_700_1k_012.ts')
-c.setopt(c.WRITEFUNCTION, buf.write)
-c.perform()
+while True:
+    buf = cStringIO.StringIO()
+    c = pycurl.Curl()
+    c.setopt(pycurl.FOLLOWLOCATION, 1)
+    c.setopt(c.URL, 'http://155.69.149.26/dream_700_700_1k_012.ts')
+    c.setopt(c.WRITEFUNCTION, buf.write)
+    c.perform()
 
-m = hashlib.md5()
-m.update(buf.getvalue())
-buf.close()
-print m.hexdigest()
+    m = hashlib.md5()
+    m.update(buf.getvalue())
+    buf.close()
+    print m.hexdigest()
