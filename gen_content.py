2fa46830254c3184667ca0388d37336e908de711
gaoguanyu <springaction@foxmail.com>
2014-09-20 16:00:43 +0800

    nw file

---
+++ gen_content.py
@@ -0,0 +1,17 @@
+import random
+
+content_num = 100
+
+source_path = ""
+source_id   = ""
+source_start = 1 * 6
+source_end = 10 * 6
+
+for no in
+
+seg_num = random.randint(source_start, source_end)
+for i in range(seg_num):
+
+    print i
+
+
