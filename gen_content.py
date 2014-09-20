ba3ee22743dbb91a87624a4038e44411245b1e32
gaoguanyu <springaction@foxmail.com>
2014-09-20 21:24:50 +0800

    dd

--- gen_content.py
+++ gen_content.py
@@ -14,13 +14,16 @@ dest_path = "/tmp"
 for content in range(0, content_num):
     content = str(content)
     new_dir = os.path.join(dest_path, content)
-    if not os.path.exists(new_dir):
-        os.makedirs(new_dir)
+    if os.path.exists(new_dir):
+        shutil.rmtree(new_dir)
+
+    os.makedirs(new_dir)
 
     seg_num = random.randint(source_start, source_end)
     print "new content id:", content
     print "segment number:", seg_num
     print "------------------------"
+
 
     for seg in range(seg_num):
         seg = str(seg)
