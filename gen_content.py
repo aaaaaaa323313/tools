d74dc37778ef72e98d4ebeeba6615ad2e9bcbb20
gaoguanyu <springaction@foxmail.com>
2014-09-20 20:58:58 +0800

    ddd

--- gen_content.py
+++ gen_content.py
@@ -1,17 +1,35 @@
+import os
 import random
+import shutil
 
 content_num = 100
 
-source_path = ""
-source_id   = ""
+source_path = "/home/guanyu/Public/me/static/lvjuren"
+source_id   = "lvjuren"
 source_start = 1 * 6
 source_end = 10 * 6
 
-for no in
+dest_path = "/tmp"
 
-seg_num = random.randint(source_start, source_end)
-for i in range(seg_num):
+for content in range(0, content_num):
+    content = str(content)
+    new_dir = os.path.join(dest_path, content)
+    if not os.path.exists(new_dir):
+        os.makedirs(new_dir)
 
-    print i
+    seg_num = random.randint(source_start, source_end)
+    print "new content id:", content
+    print "segment number:", seg_num
+    print "------------------------"
+
+    for seg in range(seg_num):
+        seg = str(seg)
+        new_seg = content + "_" + seg + ".ts"
+        new_seg = os.path.join(new_dir, new_seg)
+
+        old_seg = source_id + "_" + seg + ".ts"
+        old_seg = os.path.join(source_path, old_seg)
+
+        shutil.copy2(old_seg, new_seg)
 
 
