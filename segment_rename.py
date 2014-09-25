92273cd714161fe92f152bdd0cf7aa1e4257b055
gaoguanyu <springaction@foxmail.com>
2014-09-25 20:36:55 +0800

    ddd

--- segment_rename.py
+++ segment_rename.py
@@ -4,4 +4,8 @@ path = sys.argv[1]
 all_segments  = os.listdir(path)
 
 for segment in all_segments:
+    print segment
+    name, suffix = os.path.splitext(segment)
+    print name
+    print suffix
 
