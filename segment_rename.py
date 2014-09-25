4a1fcd5984426b3017d14cc9a1999e652a522900
gaoguanyu <springaction@foxmail.com>
2014-09-25 21:03:39 +0800

    dd

--- segment_rename.py
+++ segment_rename.py
@@ -1,11 +1,23 @@
 import os
+import sys
 
 path = sys.argv[1]
 all_segments  = os.listdir(path)
 
 for segment in all_segments:
-    print segment
-    name, suffix = os.path.splitext(segment)
-    print name
-    print suffix
+    #print "old name:", segment
 
+    name, suffix = os.path.splitext(segment)
+    if suffix != ".ts":
+        continue
+
+    [vid, sid, res] = name.split('_')
+    [wd, ht] = res.split('x')
+    new_name = vid + "_" + wd + "_" + ht + "_" + \
+            "df" + "_" + sid + suffix
+    #print new_name
+    old_path = os.path.join(path, segment)
+    print old_path
+    new_path = os.path.join(path, new_name)
+    print new_path
+    os.rename(old_path, new_path)
