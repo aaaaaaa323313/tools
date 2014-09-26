e199135ff3d172e05af60e6237b50fcd9adfb6bf
gaoguanyu <springaction@foxmail.com>
2014-09-27 02:18:24 +0800

    dd

--- simulated_users.py
+++ simulated_users.py
@@ -7,30 +7,31 @@ import threading
 import subprocess
 
 
+class viewing_thread(threading.Thread):
+
+    def __init__(self, play_list):
+        threading.Thread.__init__(self)
+        self.play_list = play_list
+
+    def run(self):
+        while True:
+            for content in self.play_list:
+                print content
+                time.sleep(3)
+
+
 r = redis.Redis(host='192.168.0.9', port=6379, db=0)
 content_num = r.get("content_num")
 content_num = int(content_num)
 
-
-
-class viewing_thread(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-        while True:
-            print "okay"
-            time.sleep(3)
-
-
 thread_num = 100
 threads = []
 
-
 try:
     for i in range(0, thread_num):
-        t = viewing_thread()
+        num_per_thread = content_num / thread_num
+        play_list = range(num_per_thread * i, num_per_thread * (i + 1) )
+        t = viewing_thread(play_list)
         t.start()
         threads.append(t)
 except:
