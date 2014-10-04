a8b24c39e7b647c06646d23d7fc5159b94d540a4
gaoguanyu <springaction@foxmail.com>
2014-10-05 01:17:55 +0800

    ddd

--- cvx_partial_caching.py
+++ cvx_partial_caching.py
@@ -11,7 +11,7 @@ def average_arrival(rate, popularity, seg, seg_num):
     return rate * popularity * math.exp((-4.6) * seg / seg_num)
 
 
-content_num = 10000
+content_num = 1000
 
 request_rates = scipy.io.loadmat('deployed_request_rates.mat')
 request_rates = request_rates['request_rate']
@@ -39,10 +39,13 @@ r.set("content_num", content_num)
 
 all_seg_num = 0
 cached_seg_num = 0
+uncached_seg_num = 0
 
 storage_prices = []
 storage_cost = []
 computing_prices = []
+computing_cost = []
+seg_req_rate = []
 segments = []
 
 m = Model("cvx123")
@@ -65,7 +68,7 @@ for i in range(0, content_num):
     r.set(key, val)
 
     key = "req_rate_" + content
-    rate = request_rates[i][0] * 4
+    rate = request_rates[i][19] * 4
     r.set(key, rate)
 
     #copy the original video content into the dir
@@ -77,12 +80,10 @@ for i in range(0, content_num):
         old_seg = source_id + "_" + seg + ".ts"
         old_seg = os.path.join(source_path, old_seg)
 
-        '''
         if os.path.isfile(new_seg):
             pass
         else:
             shutil.copy2(old_seg, new_seg)
-        '''
 
     resolutions = ['1280_720', '854_480', '640_360', '426_240']
 
@@ -95,76 +96,72 @@ for i in range(0, content_num):
             seg_tran_p = format_trans_price[j] * average_arrival(rate, format_popular[j], seg, seg_num)
 
             seg_stor_c = format_storage_size[j]
+            seg_tran_c = average_arrival(rate, format_popular[j], seg, seg_num) * trans_latency[j]
 
             storage_prices.append(seg_stor_p)
             computing_prices.append(seg_tran_p)
 
             storage_cost.append(seg_stor_c)
+            computing_cost.append(seg_tran_c)
+
+            seg_req_rate.append(average_arrival(rate, format_popular[j], seg, seg_num))
 
             seg_name = str(i) + "_" + str(j) + "_" + str(seg)
-            # print seg_name
+
             var = m.addVar(vtype=GRB.BINARY, name=seg_name)
             segments.append(var)
 
-#print len(segments)
-#print len(storage_prices)
-#print len(computing_prices)
+
+pure_storage   = sum(storage_prices)
+pure_computing = sum(computing_prices)
+total_str_c = sum(storage_cost)
+total_cmp_c = sum(computing_cost)
+total_seg_rate = sum(seg_req_rate)
 
 m.update()
-m.setObjective(LinExpr(storage_prices, segments) - LinExpr(computing_prices, segments), GRB.MINIMIZE)
-m.addConstr(LinExpr(storage_cost, segments), "<=", 10000000000, "c0")
+m.setObjective(LinExpr(storage_prices, segments) - \
+        LinExpr(computing_prices, segments) + \
+        pure_computing, GRB.MINIMIZE)
+m.addConstr(LinExpr(storage_cost, segments), "<=", total_str_c * 0.36, "c0")
+m.addConstr((total_cmp_c - LinExpr(computing_cost, segments)) \
+        / total_seg_rate, "<=", 10000000000, "c1")
 m.optimize()
 
-pure_computing = sum(computing_prices)
-pure_storage   = sum(storage_prices)
-optimal_cost   = m.objVal + pure_computing
+optimal_cost   = m.objVal
 
-print 'The cost Reduction percentage:', (pure_storage - optimal_cost) / pure_storage
 
-total = 0
-cache = 0
-miss  = 0
 for v in m.getVars():
-    #print v.varName, v.x
+    #print v.varName
+    [content, j, seg] = v.varName.split('_')
+    j = int(j)
+    seg = int(seg)
     if v.x == 1.0:
-        cache += 1
+        cached_seg_num = cached_seg_num + 1
+
+        new_seg = content + '_' + resolutions[j] + '_' + 'df' + '_' + str(seg) + '.ts'
+        new_seg = os.path.join(new_dir, new_seg)
+
+        old_seg = source_id + "_" + resolutions[j] + '_' + 'df' + '_' + str(seg) + ".ts"
+        old_seg = os.path.join(source_path, old_seg)
+
+        if os.path.isfile(new_seg):
+            pass
+        else:
+            shutil.copy2(old_seg, new_seg)
+
     elif v.x == 0.0:
-        miss += 1
+        uncached_seg_num = uncached_seg_num + 1
+        new_seg = content + '_' + resolutions[j] + '_' + 'df' + '_' + str(seg) + '.ts'
+        new_seg = os.path.join(new_dir, new_seg)
+        if os.path.isfile(new_seg):
+            os.remove(new_seg)
+
     else:
-        print "error"
+        print "solution error"
         sys.exit()
 
-print cache
-print miss
-print cache * 1.0 / (cache + miss)
-'''
-
-            if seg_tran_p > seg_stor_p:
-
-                cached_seg_num = cached_seg_num + 1
-
-                new_seg = content + '_' + resolutions[j] + '_' + 'df' + '_' + str(seg) + '.ts'
-                new_seg = os.path.join(new_dir, new_seg)
-
-                old_seg = source_id + "_" + resolutions[j] + '_' + 'df' + '_' + str(seg) + ".ts"
-                old_seg = os.path.join(source_path, old_seg)
-
-                if os.path.isfile(new_seg):
-                    pass
-                else:
-                    shutil.copy2(old_seg, new_seg)
-            else:
-                new_seg = content + '_' + resolutions[j] + '_' + 'df' + '_' + str(seg) + '.ts'
-                new_seg = os.path.join(new_dir, new_seg)
-                if os.path.isfile(new_seg):
-                    os.remove(new_seg)
-
-
-
-
-print 'cached percentage:', cached_seg_num / all_seg_num
-print 'cached number:', cached_seg_num
-print 'all number:', all_seg_num
-'''
+print '-------------------------------'
+print 'segment cached percentage:', cached_seg_num * 1.0 / all_seg_num
+print 'The cost Reduction percentage:', (pure_storage - optimal_cost) / pure_storage
 
 
